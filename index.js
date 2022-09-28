2f345e57c651475245833b1499a90efc91884bdd
Raoudha123 <112500095+Raoudha123@users.noreply.github.com>
2022-09-28 07:36:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+console.log(document)
+var btPlus = document.getElementsByClassName('plus')
+var btMinus = document.getElementsByClassName('minus')
+var total= document.getElementById('total')
+var prices=document.querySelectorAll('.price')
+var like_btns=document.querySelectorAll('.like')
+
+function sum()
+{
+var somme=0
+    for (let i=0;i<prices.length;i++)
+{
+    somme+=Number(prices[i].innerHTML)
+}
+total.innerHTML=somme
+}
+
+
+function compte(c){
+    var bt=c.target
+        var div= bt.parentElement
+        var p = div.querySelector('p')
+p.innerHTML++ 
+var quantity=Number(p.innerHTML)
+console.log(p)
+quantity++
+p.innerHTML=quantity 
+
+var td= div.parentElement
+var tr=  td.parentElement
+var unit_price= tr.querySelector('.unitPrice')
+var up=Number(unit_price.innerHTML)
+var price=tr.querySelector('.price')
+price.innerHTML=quantity*up
+sum()
+    }
+    function compteM(c){
+        var bt=c.target
+            var div= bt.parentElement
+            var p = div.querySelector('p')
+    console.log(p)
+
+    
+    if ((bt.innerHTML==="-") && (Number(p.innerHTML)>0))
+    {
+
+        quantity--
+        var quantity=Number(p.innerHTML)
+console.log(p)
+p.innerHTML=quantity 
+        var td= div.parentElement
+        var tr=  td.parentElement
+        var unit_price= tr.querySelector('.unitPrice')
+        var up=Number(unit_price.innerHTML)
+        var price=tr.querySelector('.price')
+        price.innerHTML=quantity*up
+        sum()
+    } 
+    function changeColor(e){
+        var cible=e.target
+        if (cible)
+    } 
+        }
+        for (let i=0;i<btMinus.length;i++){
+            btMinus[i].addEventListener('click',compteM)  
+        }
+        for (let i=0;i<like_btns.length;i++)
+        {
+            like_btns[i].addEventListener('click', changeColor)
+        }
+       
+       
